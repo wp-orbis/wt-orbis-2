30e085a54a8e03d8b0388d5c3dcf4c7d30d4bb5b
Karel-Jan Tolsma <kj.tolsma@gmail.com>
2015-03-24 10:59:49 +0100

    Navigation optimalisation.

--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 
 					?>
 
-					<a class="toggle-nav"><i class="fa fa-chevron-circle-left"></i> <span class="nav-label"><?php _e( 'Collapse menu', 'orbis' ); ?></span></a>
+					<a class="toggle-nav"><span class="nav-label"><?php _e( 'Collapse menu', 'orbis' ); ?></span></a>
 				</div>
 			</div>
 
